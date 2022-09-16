d8c8e1086bdcb7daeadc8d2c129368bdee47a4c8
Hebert do Nascimento <hebertn88@gmail.com>
2022-09-16 00:16:23 -0300

    Desafios 1 e 2

---
+++ src/Desafio01.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Desafio01 {
+    public static void main(String[] args) {
+        /*
+        Escreva um programa que informa a tabuada de multiplicação de um
+        número informado pelo usuário (entre 1-10). Exemplo:
+        Entre com um número de 1 a 10:
+        1
+        A tabuada de 1 é:
+        1 x 1 = 1
+        1 x 2 = 2
+        1 x 3 = 3
+        */
+        Scanner scan = new Scanner(System.in);
+
+
+        String response = "init";
+        while (!isValid(response)) {
+            System.out.println("Entre com um número de 1 a 10:");
+            response = scan.nextLine();
+
+            if (!isValid(response))
+                System.out.println("Resposta inválida! Tente novamente...");
+        }
+        int num = Integer.parseInt(response);
+
+        for (int i = 1; i <= 10; i++)
+            System.out.printf("%s X %s = %s%n",i, num, i * num);
+    }
+    public static boolean isValid(String text) {
+        if (isInt(text)) {
+            int n = Integer.parseInt(text);
+            if (n >= 1 && n <= 10)
+                return true;
+        }
+        return false;
+    }
+
+    public static boolean isInt(String text) {
+        try {
+            int n = Integer.parseInt(text);
+            return true;
+        } catch (NumberFormatException ex) {
+            return false;
+        }
+    }
+}
---
+++ src/Desafio02.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Desafio02 {
+    public static void main(String[] args) {
+        /*
+        Escreva um programa que converte temperaturas de Celsius para Fahrenheit. Fórmula (0 °C ×
+        9/5) + 32 = 32 °F. Exemplo:
+        Entre com uma temperatura em Celsius para saber seu equivalente em Fahrenheit:
+        22
+        O equivalente a 22 C é 71,6 F.
+        */
+
+        Scanner scan = new Scanner(System.in);
+
+        String response = "init";
+        while (!Desafio01.isInt(response)) {
+            System.out.println("Entre com uma temperatura em Celsius para saber seu equivalente em Fahrenheit:");
+            response = scan.nextLine();
+
+            if (!Desafio01.isInt(response))
+                System.out.println("Resposta inválida! Tente novamente...");
+        }
+        int num = Integer.parseInt(response);
+
+        System.out.printf("%s°C equivale a %s°F!%n", num, (float) num * 9 / 5 + 32);
+
+
+    }
+
+}
